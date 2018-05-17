e547957c1b6d379b69e48ee2d6f887e619b16bbb
Elliott Short <elliott.short@gmail.com>
2018-05-17 12:09:28 -0500

    Added join plot functionality.

--- src/main/java/me/overlord/bot/commands/UtilityCommands.java
+++ src/main/java/me/overlord/bot/commands/UtilityCommands.java
@@ -1,13 +1,28 @@
 package me.overlord.bot.commands;
 
-import java.awt.Color;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import me.overlord.bot.commandframework.annotation.Command;
 import me.overlord.bot.commandframework.annotation.CommandSet;
+import me.overlord.bot.util.Common;
 import me.overlord.bot.util.Constants.UserType;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.utils.MiscUtil;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import javax.imageio.ImageIO;
 
 @CommandSet
 public class UtilityCommands {
@@ -19,35 +34,18 @@ public class UtilityCommands {
         new EmbedBuilder()
             .setDescription("Status")
             .setColor(new Color(0, 255, 255))
-            .addField("Uptime", botUptime(), true)
+            .addField("Uptime", Common.botUptime(), true)
             .addField("Ping", Long.toString(event.getJDA().getPing()) + "ms", true);
 
     event.getChannel().sendMessage(statusEmbed.build()).queue();
   }
 
-  /*
-   * Utility Methods
-   */
-
-  private String botUptime() {
-    RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
-    long seconds = rb.getUptime() / 1000;
-    long d = (long) Math.floor(seconds / 86400);
-    long h = (long) Math.floor((seconds % 86400) / 3600);
-    long m = (long) Math.floor(((seconds % 86400) % 3600) / 60);
-    long s = (long) Math.floor(((seconds % 86400) % 3600) % 60);
-
-    if (d > 0) {
-      return String.format("%sd %sh %sm %ss", d, h, m, s);
-    }
-
-    if (h > 0) {
-      return String.format("%sh %sm %ss", h, m, s);
-    }
-
-    if (m > 0) {
-      return String.format("%sm %ss", m, s);
-    }
-    return String.format("%ss", s);
+  @Command(value = "plot-joins", permission = UserType.Administrator)
+  private void plot(MessageReceivedEvent event, String[] arguments) {
+    event.getChannel().sendTyping().queue();
+    event
+        .getChannel()
+        .sendFile(Common.drawPlot(event.getGuild(), event.getMessage().getCreationTime()))
+        .queue();
   }
 }
--- src/main/java/me/overlord/bot/util/Common.java
+++ src/main/java/me/overlord/bot/util/Common.java
@@ -2,13 +2,26 @@ package me.overlord.bot.util;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.utils.MiscUtil;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
 import java.net.URL;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static me.overlord.bot.App.commands;
@@ -42,5 +55,69 @@ public class Common {
                 + command
                 + "' :: please add it to the help.json file as soon as possible ***");
     }
+  }
+
+  public static String botUptime() {
+    RuntimeMXBean rb = ManagementFactory.getRuntimeMXBean();
+    long seconds = rb.getUptime() / 1000;
+    long d = (long) Math.floor(seconds / 86400);
+    long h = (long) Math.floor((seconds % 86400) / 3600);
+    long m = (long) Math.floor(((seconds % 86400) % 3600) / 60);
+    long s = (long) Math.floor(((seconds % 86400) % 3600) % 60);
+
+    if (d > 0) {
+      return String.format("%sd %sh %sm %ss", d, h, m, s);
+    }
+
+    if (h > 0) {
+      return String.format("%sh %sm %ss", h, m, s);
+    }
+
+    if (m > 0) {
+      return String.format("%sm %ss", m, s);
+    }
+    return String.format("%ss", s);
+  }
+
+  public static File drawPlot(Guild guild, OffsetDateTime now) {
+    long start = MiscUtil.getCreationTime(guild.getIdLong()).toEpochSecond();
+    long end = now.toEpochSecond();
+    int width = 1000;
+    int height = 600;
+    List<Member> joins = new ArrayList<>(guild.getMembers());
+    Collections.sort(joins, (Member a, Member b) -> a.getJoinDate().compareTo(b.getJoinDate()));
+    BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB_PRE);
+    Graphics2D g2d = bi.createGraphics();
+    g2d.setComposite(AlphaComposite.SrcOver);
+    g2d.setColor(new Color(39, 51, 71));
+    g2d.fillRect(0, 0, width, height);
+    double lastX = 0;
+    int lastY = height;
+
+    for (int i = 0; i < joins.size(); i++) {
+      double x = (((joins.get(i).getJoinDate()).toEpochSecond() - start) * width) / (end - start);
+      int y = height - ((i * height) / joins.size());
+      double angle =
+          (x == lastX) ? 1.0 : Math.tan((double) (lastY - y) / (x - lastX)) / (Math.PI / 2);
+      g2d.setColor(Color.getHSBColor((float) angle / 4, 1.0f, 1.0f));
+      g2d.drawLine((int) x, y, (int) lastX, lastY);
+      lastX = x;
+      lastY = y;
+    }
+    g2d.setFont(g2d.getFont().deriveFont(24f));
+    g2d.setColor(Color.WHITE);
+    g2d.drawString("0 - " + joins.size() + " Users", 20, 26);
+    g2d.drawString(
+        MiscUtil.getCreationTime(guild.getIdLong()).format(DateTimeFormatter.RFC_1123_DATE_TIME),
+        20,
+        60);
+    g2d.drawString(now.format(DateTimeFormatter.RFC_1123_DATE_TIME), 20, 90);
+    File joinPlot = new File("plot.png");
+    try {
+      ImageIO.write(bi, "png", joinPlot);
+    } catch (IOException ex) {
+      logger.error("Error occured generating the plot :: " + ex.getLocalizedMessage());
+    }
+    return joinPlot;
   }
 }
